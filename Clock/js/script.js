c9f893326cc0ac22eba633b92e4874ab4401cb9b
Evy <evylukassen@hotmail.com>
2020-10-30 14:54:42 +0100

    Time update
    The time will now be updated every second. The image will change at different hour times.

--- Clock/js/script.js
+++ Clock/js/script.js
@@ -1,3 +1,50 @@
-// Ophalen van class waar image in staat
-// Ophalen van de class waar tijd in staat
-// Tussen .. en .. dan wordt ...png weergegeven.
+//First the you get the elements and put them in an variable.
+var Img = document.getElementById("Image");
+var TimeandDate = document.getElementById("TimeAndDate")
+
+//function that get's the current time.
+function GetTime(){   
+    //Getting the current time
+    var newDate = new Date();
+    var Time = newDate.toLocaleTimeString();
+    var currentTime = newDate.getHours();
+
+    //Displaying time
+    TimeandDate.innerHTML = Time;
+
+    //With these if statemnts the time is checked to see wich picture will
+    //be displayed. I need to come back to this to code it better.
+    if(currentTime >= "8" && currentTime < "9") {
+        Img.src="img/coffee.png";
+    }
+
+    else if(currentTime >= "9" && currentTime < "12") {
+        Img.src="img/graduation-hat.png";
+    }
+
+    else if(currentTime >= "12" && currentTime < "13") {
+        Img.src="img/take-away.png";
+    }
+
+    else if(currentTime >= "13" && currentTime < "16") {
+        Img.src="img/graduation-hat.png";
+    }
+
+    else if(currentTime >= "16" && currentTime < "18") {
+        Img.src="img/study.png";
+    }
+
+    else if(currentTime >= "18" && currentTime < "19") {
+        Img.src="img/eat.png";
+    }
+
+    else if(currentTime >= "19" && currentTime < "23") {
+        Img.src="img/joystick.png";
+    }
+
+    else {
+        Img.src="img/moon.png";
+    }
+}
+//refresh time every second.
+setInterval(GetTime, 1000);
